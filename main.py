8ed73a00531de977eebde2567c98d16812fdf50a
Ihor Vasylets <44103737+SuitWhite@users.noreply.github.com>
2021-09-16 17:24:55 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def fac(n):
-    if (n == 0 or n<1):
-        return 1
-    return fac(n-1) * n
-print("Enter k:")
-k = int(input())
-result = fac(k//10)*fac(k%10)
-print("For k you have ", result, "variants")
