7f3c13ef23a8ae88854bc041de8a8428ef235c3c
Ihor Vasylets <44103737+SuitWhite@users.noreply.github.com>
2021-09-28 22:49:20 +0300

    Завдання 1, НП, 1.10

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ def fac(n):
     if (n == 0 or n<1):
         return 1
     return fac(n-1) * n
-print("Enter k:")
-k = int(input())
-result = fac(k//10)*fac(k%10)
-print("For k you have ", result, "variants")
+
+class MyException(Exception): pass
+while True:
+        try:
+            print("Enter k:")
+            k = int(input())
+            if k<=0:
+                raise MyException
+            result = fac(k//10)*fac(k%10)
+            print("For k you have ", result, "variants")
+        except MyException:
+            print("k<1")
+            continue
+        except ValueError:
+            print("Value should be int")
+            continue
