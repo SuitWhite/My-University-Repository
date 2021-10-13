d1e84d2151c29637265e4f2a0ca65e4c2f8a89d5
Ihor Vasylets <44103737+SuitWhite@users.noreply.github.com>
2021-10-14 01:24:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-class MyException(Exception): pass
+class MyException(Exception):
+    pass
 
 
 def input_positive_number():
@@ -16,19 +16,25 @@ def input_positive_number():
             print("Value should be int")
             continue
 
-def count1(n,m):
+
+def count1(n, m):
     count = 0
-    if (m>0 and n-1>=0):
+    if m > 0 and n - 1 >= 0:
         count += count1(n - 1, m - 1)
-    if (n-10>=0):
-        count+= count1(n-10,3)
-    if (n==0):
+    if n - 10 >= 0:
+        count += count1(n - 10, 3)
+    if n == 0:
         return 1
     return count
 
-if __name__ == '__main__':
+
+def main():
     while True:
         print("Введіть розмір конструкції:")
         size = input_positive_number()
-        count = count1(size,3)
-        print(f'Результат: {count}' )
+        count = count1(size, 3)
+        print(f'Результат: {count}')
+
+
+if __name__ == '__main__':
+    main()
